da57eb06bd5257f3dadcc6be1512326fb449191f
skye <67734042+sky-larke@users.noreply.github.com>
2025-06-03 23:46:09 -0500

    hotfix

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,4 @@
 import type { LayoutServerLoad } from './$types'
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
-import { createServerClient } from '@supabase/ssr'
-import type { Handle } from '@sveltejs/kit'
 
 export const load: LayoutServerLoad = async ({ locals: { safeGetSession }, cookies }) => {
   const { session } = await safeGetSession()
@@ -9,28 +6,4 @@ export const load: LayoutServerLoad = async ({ locals: { safeGetSession }, cooki
     session,
     cookies: cookies.getAll(),
   }
-}
-
-export const handle: Handle = async ({ event, resolve }) => {
-  event.locals.supabase = createServerClient(
-    PUBLIC_SUPABASE_URL,
-    PUBLIC_SUPABASE_ANON_KEY,
-    {
-      cookies: {
-        get: (key) => event.cookies.get(key),
-        set: (key, value, options) => {
-          event.cookies.set(key, value, options)
-        },
-        remove: (key, options) => {
-          event.cookies.delete(key, options)
-        }
-      }
-    }
-  )
-
-  return resolve(event, {
-    filterSerializedResponseHeaders(name) {
-      return name === 'content-range'
-    }
-  })
 }
